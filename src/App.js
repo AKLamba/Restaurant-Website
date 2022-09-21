4c2edf2fd70df1958dcfcc06f0b937615838ad63
AKLamba <ce20b007@smail.iitm.ac.in>
2022-09-21 23:38:42 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Resturant from './component/Basics/Resturant';
+const App = () => {
+  return (
+    <>
+    <Resturant/>
+    </>
+  );
+};
+
+const Name = () => {
+  return(
+    <h1>Hello Everyone</h1>
+  )
+}
+
+export default App
